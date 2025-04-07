6a2aa9d5f75ee7d5d976ba14106b00f197f67ecf
Cokke <jorgerandomgamer@gmail.com>
2025-04-07 12:36:49 -0400

    Agregado cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,39 @@
+import { TimeOutInterceptor } from './common/interceptors/timeout.interceptor';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionFilter } from './common/filters/http-exception.filter';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const cors = require('cors')
+  app.useGlobalFilters(new AllExceptionFilter());
+  app.useGlobalInterceptors(new TimeOutInterceptor());
+  app.useGlobalPipes(new ValidationPipe());
+  // Configurar CORS usando el middleware
+  app.use(cors({
+    origin: [process.env.FRONT_URL],
+    methods: ['GET', 'POST','PUT','DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  }));
+
+  const options = new DocumentBuilder()
+    .setTitle('Proyección API')
+    .setDescription('Proyecto Minutas App')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('/api/docs', app, document, {
+    swaggerOptions: {
+      filter: true,
+    },
+  });
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
