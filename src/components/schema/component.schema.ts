dd5fa3d308803c011da61f3e13cfd707320af8ee
Constanza Vázquez <cvazqueztes@gmail.com>
2025-05-05 18:08:17 -0400

    corrección schema componentes

--- src/components/schema/component.schema.ts
+++ src/components/schema/component.schema.ts
@@ -5,12 +5,7 @@ export const ComponentSchema = new mongoose.Schema(
     name: { type: String, required: true },
     type: { type: String, required: true },
     status: { type: String, enum: ['activo', 'de baja'], default: 'activo' },
-    descriptions: [
-      {
-        name: { type: String, required: true },
-        description: { type: String, required: true },
-      }
-    ],
+    descriptions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Description',}],
     components: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Component' }],
   },
   { timestamps: true }
